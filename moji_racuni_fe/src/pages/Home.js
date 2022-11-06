c1fb1016e342d9d4e1366beebb4f12d6c4fe31f0
ivang5 <ivanprogaborov@gmail.com>
2022-11-06 19:30:54 +0100

    [MR-16] Kreiranje btn stilova

--- moji_racuni_fe/src/pages/Home.js
+++ moji_racuni_fe/src/pages/Home.js
@@ -30,6 +30,24 @@ const Home = () => {
       ) : (
         <p>Empty</p>
       )}
+      <a href="#" className="btn btn-primary btn-round">
+        Primary
+      </a>
+      <a href="#" className="btn btn-secondary btn-round">
+        Secondary
+      </a>
+      <a href="#" className="btn btn-primary btn-primary--white btn-round">
+        White
+      </a>
+      <a href="#" className="btn btn-secondary btn-secondary--white btn-round">
+        White
+      </a>
+      <a href="#" className="btn btn-primary btn-round btn-lg">
+        Primary
+      </a>
+      <a href="#" className="btn btn-secondary btn-round btn-lg">
+        Secondary
+      </a>
     </div>
   );
 };
