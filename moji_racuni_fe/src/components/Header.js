2094ea36277a594d80bb4ebaef013d86527948cf
ivang5 <ivanprogaborov@gmail.com>
2022-11-11 20:58:12 +0100

    [MR-18] Kreiranje login stranice

--- moji_racuni_fe/src/components/Header.js
+++ moji_racuni_fe/src/components/Header.js
@@ -5,32 +5,72 @@ import AuthContext from "../context/AuthContext";
 const Header = () => {
   const { user, logoutUser } = useContext(AuthContext);
   const [mobileNav, setMobileNav] = useState(false);
+  const currentPath = window.location.pathname;
 
   const toggleMobileNav = () => {
     setMobileNav(!mobileNav);
   };
 
   return (
-    <div>
-      <header className={mobileNav ? "header header--active" : "header"}>
-        <div className="l-container p-1">
-          <nav className="nav">
-            <div className="nav__brand d-inline">
-              <Link className="nav__logo" to="/">
-                <img
-                  className="nav__logo-img"
-                  src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Approve_icon.svg"
-                  alt="logo"
-                />
-              </Link>
-              <span
-                className={
-                  mobileNav ? "nav__burger nav__burger--active" : "nav__burger"
-                }
-                onClick={toggleMobileNav}
-              ></span>
+    <>
+      {currentPath !== "/login" && user !== null && (
+        <div>
+          <header className={mobileNav ? "header header--active" : "header"}>
+            <div className="l-container p-1">
+              <nav className="nav">
+                <div className="nav__brand d-inline">
+                  <Link className="nav__logo" to="/">
+                    <img
+                      className="nav__logo-img"
+                      src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Approve_icon.svg"
+                      alt="logo"
+                    />
+                  </Link>
+                  <span
+                    className={
+                      mobileNav
+                        ? "nav__burger nav__burger--active"
+                        : "nav__burger"
+                    }
+                    onClick={toggleMobileNav}
+                  ></span>
+                </div>
+                <ul className="nav__list">
+                  <li className="nav__list-item">
+                    <Link className="nav__link" data-content="Home" to="/">
+                      Home
+                    </Link>
+                  </li>
+                  <li className="nav__list-item">
+                    <Link
+                      className="nav__link nav__link-active"
+                      data-content="About"
+                      to="/"
+                    >
+                      About
+                    </Link>
+                  </li>
+                  <li className="nav__list-item">
+                    <Link className="nav__link" data-content="Contact" to="/">
+                      Contact
+                    </Link>
+                  </li>
+                </ul>
+                <div className="nav__logout" onClick={logoutUser}>
+                  <span className="nav__link" data-content="Logout">
+                    Logout
+                  </span>
+                </div>
+              </nav>
+              {/* {user && <span className="pl-1">Hello {user.username}</span>} */}
             </div>
-            <ul className="nav__list">
+          </header>
+          <div
+            className={
+              mobileNav ? "mobile-nav mobile-nav--active" : "mobile-nav"
+            }
+          >
+            <ul className="mobile-nav__list">
               <li className="nav__list-item">
                 <Link className="nav__link" data-content="Home" to="/">
                   Home
@@ -50,49 +90,18 @@ const Header = () => {
                   Contact
                 </Link>
               </li>
+              <li>
+                <div className="mobile-nav__logout" onClick={logoutUser}>
+                  <span className="nav__link" data-content="Logout">
+                    Logout
+                  </span>
+                </div>
+              </li>
             </ul>
-            <div className="nav__logout" onClick={logoutUser}>
-              <span className="nav__link" data-content="Logout">
-                Logout
-              </span>
-            </div>
-          </nav>
-          {/* {user && <span className="pl-1">Hello {user.username}</span>} */}
+          </div>
         </div>
-      </header>
-      <div
-        className={mobileNav ? "mobile-nav mobile-nav--active" : "mobile-nav"}
-      >
-        <ul className="mobile-nav__list">
-          <li className="nav__list-item">
-            <Link className="nav__link" data-content="Home" to="/">
-              Home
-            </Link>
-          </li>
-          <li className="nav__list-item">
-            <Link
-              className="nav__link nav__link-active"
-              data-content="About"
-              to="/"
-            >
-              About
-            </Link>
-          </li>
-          <li className="nav__list-item">
-            <Link className="nav__link" data-content="Contact" to="/">
-              Contact
-            </Link>
-          </li>
-          <li>
-            <div className="mobile-nav__logout" onClick={logoutUser}>
-              <span className="nav__link" data-content="Logout">
-                Logout
-              </span>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
--- moji_racuni_fe/src/pages/Login.js
+++ moji_racuni_fe/src/pages/Login.js
@@ -1,17 +1,215 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import AuthContext from "../context/AuthContext";
 
 const Login = () => {
   const { loginUser } = useContext(AuthContext);
+  const [registration, setRegistration] = useState(false);
+
+  const goToLogin = () => {
+    setRegistration(false);
+  };
+
+  const goToRegistration = () => {
+    setRegistration(true);
+  };
 
   return (
-    <div className="mt-5">
-      <form onSubmit={loginUser}>
-        <input type="text" name="username" placeholder="Enter Username" />
-        <input type="password" name="password" placeholder="Enter Password" />
-        <input type="submit" />
-      </form>
-    </div>
+    <>
+      <div className="logreg">
+        <div
+          className={
+            registration
+              ? "logreg__login-aside"
+              : "logreg__login-aside logreg__login-aside--active"
+          }
+        >
+          <div className="logreg__aside-content">
+            <h2 className="logreg__aside-title capitalize">Nemate nalog?</h2>
+            <p className="logreg__aside-text">
+              Ako još niste kreirali nalog, registrujte se.
+            </p>
+            <button
+              className="btn btn-lg btn-secondary btn-secondary--white btn-round"
+              onClick={goToRegistration}
+            >
+              Registrujte se
+            </button>
+          </div>
+          <div className="logreg__shape-wrapper">
+            <div className="logreg__shape logreg__shape--1"></div>
+            <div className="logreg__shape logreg__shape--2"></div>
+            <div className="logreg__shape logreg__shape--3"></div>
+          </div>
+        </div>
+        <div
+          className={
+            registration
+              ? "logreg__form-wrapper logreg__form-wrapper--reg"
+              : "logreg__form-wrapper"
+          }
+        >
+          <div
+            className={
+              registration
+                ? "logreg__form"
+                : "logreg__form logreg__form--active"
+            }
+          >
+            <div>
+              <h1 className="logreg__form-title capitalize">
+                Prijavite se na svoj nalog
+              </h1>
+              <span className="logreg__form-title-line"></span>
+            </div>
+            <form className="form pt-3" onSubmit={loginUser}>
+              <div className="form__group">
+                <label className="form__label" htmlFor="username">
+                  <input
+                    className="form__input"
+                    id="username"
+                    type="text"
+                    name="username"
+                    placeholder=" "
+                  />
+                  <span className="form__label-text">Korisničko ime</span>
+                </label>
+              </div>
+              <div className="form__group">
+                <label className="form__label" htmlFor="password">
+                  <input
+                    className="form__input"
+                    id="password"
+                    type="password"
+                    name="password"
+                    placeholder=" "
+                  />
+                  <span className="form__label-text">Lozinka</span>
+                </label>
+              </div>
+              <button className="btn btn-primary btn-round" type="submit">
+                Prijava
+              </button>
+            </form>
+          </div>
+          <div
+            className={
+              registration
+                ? "logreg__form logreg__form--active"
+                : "logreg__form"
+            }
+          >
+            <div>
+              <h1 className="logreg__form-title capitalize">Kreirajte nalog</h1>
+              <span className="logreg__form-title-line"></span>
+            </div>
+            <form className="form" onSubmit={loginUser}>
+              <div className="form__group-wrapper">
+                <div className="form__group">
+                  <label className="form__label" htmlFor="reg-name">
+                    <input
+                      className="form__input"
+                      id="reg-name"
+                      type="text"
+                      name="name"
+                      placeholder=" "
+                    />
+                    <span className="form__label-text">Ime</span>
+                  </label>
+                </div>
+                <div className="form__group">
+                  <label className="form__label" htmlFor="reg-lastname">
+                    <input
+                      className="form__input"
+                      id="reg-lastname"
+                      type="text"
+                      name="lastname"
+                      placeholder=" "
+                    />
+                    <span className="form__label-text">Prezime</span>
+                  </label>
+                </div>
+                <div className="form__group">
+                  <label className="form__label" htmlFor="reg-email">
+                    <input
+                      className="form__input"
+                      id="reg-email"
+                      type="email"
+                      name="email"
+                      placeholder=" "
+                    />
+                    <span className="form__label-text">Email</span>
+                  </label>
+                </div>
+                <div className="form__group">
+                  <label className="form__label" htmlFor="reg-username">
+                    <input
+                      className="form__input"
+                      id="reg-username"
+                      type="text"
+                      name="username"
+                      placeholder=" "
+                    />
+                    <span className="form__label-text">Korisničko ime</span>
+                  </label>
+                </div>
+                <div className="form__group">
+                  <label className="form__label" htmlFor="reg-password">
+                    <input
+                      className="form__input"
+                      id="reg-password"
+                      type="password"
+                      name="password"
+                      placeholder=" "
+                    />
+                    <span className="form__label-text">Lozinka</span>
+                  </label>
+                </div>
+                <div className="form__group">
+                  <label className="form__label" htmlFor="reg-password-1">
+                    <input
+                      className="form__input"
+                      id="reg-password-1"
+                      type="password"
+                      name="password-1"
+                      placeholder=" "
+                    />
+                    <span className="form__label-text">Ponovite lozinku</span>
+                  </label>
+                </div>
+              </div>
+              <button className="btn btn-primary btn-round" type="submit">
+                Registracija
+              </button>
+            </form>
+          </div>
+        </div>
+        <div
+          className={
+            registration
+              ? "logreg__reg-aside logreg__reg-aside--active"
+              : "logreg__reg-aside"
+          }
+        >
+          <div className="logreg__aside-content">
+            <h2 className="logreg__aside-title capitalize">Imate nalog?</h2>
+            <p className="logreg__aside-text">
+              Ako ste već registorovani, samo se prijavite. Dobrodošli nazad!
+            </p>
+            <button
+              className="btn btn-lg btn-secondary btn-secondary--white btn-round"
+              onClick={goToLogin}
+            >
+              Prijavite se
+            </button>
+          </div>
+          <div className="logreg__shape-wrapper">
+            <div className="logreg__shape logreg__shape--4"></div>
+            <div className="logreg__shape logreg__shape--5"></div>
+            <div className="logreg__shape logreg__shape--6"></div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
